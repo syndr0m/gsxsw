97941f78e431b2d740e6cebd5eab98ab86b48463
Marc Dassonneville <marc.dassonneville@gmail.com>
2011-02-13 14:03:54 +0100

    some refactoring, adding basic GUI.

---
+++ static/js/livenow.jquery.js
@@ -0,0 +1,29 @@
+(function($) {
+   var socket;
+
+   $.fn.extend({ 
+      livenow: function(channelName) {
+         // FIXME
+         var host = new String(document.location).split('/')[2];
+      
+         // using custom events.
+         socket = new io.Socket(host, { port: 80 } );
+         socket.connect();
+         // INPUTS
+         socket.on('connect', function(){ $(document).trigger("connected"); });
+         socket.on('message', function(data){
+            console.log('recieved: '+data);
+            $(document).trigger("message", JSON.parse(data));
+         });
+         socket.on('disconnect', function(){ $(document).trigger("disconnected"); });
+
+         // OUTPUTS
+         $(document).bind("send", function (e) {
+            var data = [];
+            for (i in arguments) { if (i > 0) { data.push(arguments[i]); }}
+            console.log('send: '+JSON.stringify(data));
+            socket.send(JSON.stringify(data));
+         });
+      },
+   });
+})(jQuery);
